a5afff65f0e8e8fa3245ef3f4193b26497b2550e
Masoud <masuodmohararzadeh@gmail.com>
2025-07-16 02:20:39 +0330

    missed a comment...

--- Fluent-Python/01-vector-class.py
+++ Fluent-Python/01-vector-class.py
@@ -3,6 +3,8 @@
 
 from math import hypot
 
+# Class to represent 2-dimension vectors
+# You can do this with complex type too
 class Vector:
 
     def __init__(self, x=0, y=0):
@@ -12,7 +14,6 @@ class Vector:
     # __repr__ is for debugging/programmer understanding
     # eval(repr(object)) --> debugging
     def __repr__(self):
-        # %r is a place holder
         # return 'Vector(%r, %r)' % (self.x, self.y)
         # You can write it both ways
         return f'Vector({self.x}, {self.y})'
@@ -23,8 +24,6 @@ class Vector:
 
     def __bool__(self):
         return bool(abs(self))
-        # or which is faster --> cause there is no abs() anymore
-        # return bool(self.x or self.y)
 
     def __add__(self, other):
         x = self.x + other.x
@@ -56,3 +55,5 @@ Vector(9, 12)
 if __name__=='__main__':
     import doctest
     doctest.testmod()
+    # If you get nothing printed when you execute this code, it means everything works fine the way you want
+    # No massage shown = Exception == reality
